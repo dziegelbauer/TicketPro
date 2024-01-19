1c86103ab8d433d06fed0e72000b9387a7d5f675
David Ziegelbauer <dziegelbauer@gmail.com>
2024-01-18 20:48:13 -0700

    Added unit test project

---
+++ TicketProTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TicketProTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
