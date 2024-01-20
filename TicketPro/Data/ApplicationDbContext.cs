a99a55708a14b6f0489835afb7fffae069efe93a
David Ziegelbauer <dziegelbauer@gmail.com>
2024-01-20 13:12:59 -0700

    Added seeded roles.

--- TicketPro/Data/ApplicationDbContext.cs
+++ TicketPro/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using TicketPro.Data.Models;
@@ -10,4 +11,31 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<Ticket> Tickets => Set<Ticket>();
     public DbSet<TicketUpdate> TicketUpdates => Set<TicketUpdate>();
     public DbSet<Customer> Customers => Set<Customer>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityRole>().HasData(
+            new IdentityRole
+            {
+                Id = "104B3E6E-74E7-4CD5-B3BD-2D5567AE4101",
+                Name = "Admin"
+            },
+            new IdentityRole
+            {
+                Id = "A83849D4-4393-4930-9C1A-319AA1572F59",
+                Name = "Executive"
+            },
+            new IdentityRole
+            {
+                Id = "3D8EEDC8-9B0C-4921-9681-4A55D35BD5DD",
+                Name = "Manager"
+            },
+            new IdentityRole
+            {
+                Id = "F9D9FA29-3F33-4C3C-9CA2-A51B11581DB7",
+                Name = "Technician"
+            });
+    }
 }
